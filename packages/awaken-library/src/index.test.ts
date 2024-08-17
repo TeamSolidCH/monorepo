8021bbbebd2854e5b4cac4ef093ea456d4a850ef
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-08-17 11:27:52 +0200

    add test to library

---
+++ packages/awaken-library/src/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "bun:test";
+
+describe("GetIndexTest", () => {
+    it('Should return "Hello Hono!"', async () => {
+        expect("Hello Hono!").toBe("Hello Hono!");
+    });
+});
