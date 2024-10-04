b61d1c8f8069bc2a89b2792264bd09a18cdedb17
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-10-04 13:11:47 +0200

    formtat

--- crates/calendarbot/src/discord/calendar_event.rs
+++ crates/calendarbot/src/discord/calendar_event.rs
@@ -1,11 +1,11 @@
 use crate::discord::{Discord, LocalCache};
 use crate::UpdateCalendarEvent;
 
-use anyhow::Result;
-use diesel::r2d2::{ConnectionManager, Pool};
-use crate::schema::guilds_calendars::dsl as guilds_calendars;
 use crate::schema::calendars::dsl as calendars;
+use crate::schema::guilds_calendars::dsl as guilds_calendars;
+use anyhow::Result;
 use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::PgConnection;
 use google_calendar3::api::Event;
 use google_calendar3::chrono::{Datelike, NaiveDate, NaiveTime, Utc};
@@ -17,38 +17,41 @@ use tokio::sync::{mpsc, Mutex};
 
 impl Discord {
     /// Send or edit a message in a channel
-    async fn send_or_edit_message(channel_id: u64, message_id: Option<u64>, embed: serenity::CreateEmbed, cache: LocalCache) -> Result<serenity::MessageId>
-    {
-      let channel = serenity::ChannelId::new(channel_id);
-      if let Some(message_id) = message_id {
-        let msg_id = serenity::MessageId::new(message_id);
-        debug!("Trying to edit message ({})", msg_id.get());
-        let result = cache
-                        .client
-                        .edit_message(
-                            channel,
-                            msg_id,
-                            &serenity::EditMessage::new().add_embed(embed.clone()),
-                            Vec::new(),
-                        )
-                        .await;
+    async fn send_or_edit_message(
+        channel_id: u64,
+        message_id: Option<u64>,
+        embed: serenity::CreateEmbed,
+        cache: LocalCache,
+    ) -> Result<serenity::MessageId> {
+        let channel = serenity::ChannelId::new(channel_id);
+        if let Some(message_id) = message_id {
+            let msg_id = serenity::MessageId::new(message_id);
+            debug!("Trying to edit message ({})", msg_id.get());
+            let result = cache
+                .client
+                .edit_message(
+                    channel,
+                    msg_id,
+                    &serenity::EditMessage::new().add_embed(embed.clone()),
+                    Vec::new(),
+                )
+                .await;
 
-        if let Err(e) = result {
-                        error!("Failed to edit message ({}): {}", message_id.clone(), e);
-                    } else {
-                        return Ok(msg_id);
-                    }
-      }
-        
+            if let Err(e) = result {
+                error!("Failed to edit message ({}): {}", message_id.clone(), e);
+            } else {
+                return Ok(msg_id);
+            }
+        }
+
         debug!("Send new message");
         match channel
             .send_message(cache, serenity::CreateMessage::new().add_embed(embed))
-            .await {
-                Err(e) => Err(e.into()),
-                Ok(res) => Ok(res.id)
-            }
-
-        
+            .await
+        {
+            Err(e) => Err(e.into()),
+            Ok(res) => Ok(res.id),
+        }
     }
 
     pub(crate) fn calendar_events_thread(
@@ -79,30 +82,40 @@ impl Discord {
                         true,
                     ),
                 };
-                
+
                 for (channel_id, message_id) in event.discord_channel_and_message_ids {
                     debug!(target: &channel_id.to_string(), "Handling new_events with message_id: {:?}", message_id);
-                    let result = Discord::send_or_edit_message(channel_id, message_id, embed.clone(), cache.clone()).await;
+                    let result = Discord::send_or_edit_message(
+                        channel_id,
+                        message_id,
+                        embed.clone(),
+                        cache.clone(),
+                    )
+                    .await;
 
                     match result {
                         Err(e) => {
                             error!("Failed to send or edit message: {}, calendar: {}, channel_id: {:?}, message_id: {:?}", e, event.calendar_id, channel_id, message_id);
-                        },
+                        }
                         Ok(msg_id) => {
                             // We need to update the message_id in the database
                             if Some(msg_id.get()) != message_id {
-                                let db_cal_id = calendars::calendars.filter(calendars::googleid.eq(event.calendar_id.clone()))
+                                let db_cal_id = calendars::calendars
+                                    .filter(calendars::googleid.eq(event.calendar_id.clone()))
                                     .select(calendars::id)
                                     .first::<i32>(&mut db)
                                     .expect("Unable to get calendar id from database");
-                                                                    
 
                                 diesel::update(guilds_calendars::guilds_calendars)
-                                    .filter(guilds_calendars::channelid.eq(channel_id.to_string()).and(guilds_calendars::calendar_id.eq(db_cal_id)))
+                                    .filter(
+                                        guilds_calendars::channelid
+                                            .eq(channel_id.to_string())
+                                            .and(guilds_calendars::calendar_id.eq(db_cal_id)),
+                                    )
                                     .set(guilds_calendars::messageid.eq(msg_id.get().to_string()))
                                     .execute(&mut db)
                                     .expect("Unable to update message id in database");
-                            }                                        
+                            }
                         }
                     };
                 }
