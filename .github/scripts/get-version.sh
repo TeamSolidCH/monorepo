e81c49b244ed61fb3c6880aae4633443aec5f1f7
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-10-18 16:33:05 +0200

    try fix

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -3,13 +3,18 @@ if [[ "$GITHUB_EVENT_NAME" == "$push_string" ]]; then
 	echo "Run for tag"
 	tag_name=$GITHUB_REF_NAME
 	echo "Tag name: $tag_name"
-	VERSION=$(echo $STRING | rev | cut -d'/' -f1 | rev)
+	VERSION=$(echo $tag_name | rev | cut -d'/' -f1 | rev)
 	echo "Got version: $VERSION"
 	echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
 else
 	echo "Run for pull_request" 
 	echo "Got pre-release: pr-$PR_NUMBER"
-	VERSION="$LAST_TAG-pr-$PR_NUMBER"
+	last_tag = $(git describe --tags --abbrev=0 --match="calendarbot/*")
+	if [ -z "$last_tag" ]; then
+		echo "No tags found defaulting to calendarbot/0.0.1"
+		last_tag = "calendarbot/0.0.1"
+	fi
+	VERSION="$(echo $last_tag | rev | cut -d'/' -f1 | rev)-pr-$PR_NUMBER"
 	echo "Got version: $VERSION"
 	echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
 fi
