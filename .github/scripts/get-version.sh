98bf395261688cb3e790438602e429fbc7db41b0
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-10-19 21:51:01 +0200

    add rc for main branch and add a latest tag when deploying from tag

--- .github/scripts/get-version.sh
+++ .github/scripts/get-version.sh
@@ -1,12 +1,26 @@
 push_string="push"
+pull_request_string="pull_request"
 if [[ "$GITHUB_EVENT_NAME" == "$push_string" ]]; then
-	echo "Run for tag"
-	tag_name=$GITHUB_REF_NAME
-	echo "Tag name: $tag_name"
-	VERSION=$(echo $tag_name | rev | cut -d'/' -f1 | rev)
-	echo "Got version: $VERSION"
-	echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
-else
+	if [[ $GITHUB_REF == refs/tags/* ]]; then
+		echo "Run for push tag"
+		tag_name=$GITHUB_REF_NAME
+		echo "Tag name: $tag_name"
+		VERSION=$(echo $tag_name | rev | cut -d'/' -f1 | rev)
+		echo "Got version: $VERSION"
+		echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
+		echo "TAG_LATEST=true" >> $GITHUB_OUTPUT
+	elif [[ $GITHUB_REF == refs/heads/main ]]; then
+		echo "Run for push to main"
+		last_tag=$(git describe --tags --abbrev=0 --match="calendarbot/*")
+		if [ -z "$last_tag" ]; then
+			echo "No tags found defaulting to calendarbot/v0.0.1"
+			last_tag="calendarbot/v0.0.1"
+		fi
+		VERSION="$(echo $last_tag | rev | cut -d'/' -f1 | rev)-rc.$GITHUB_RUN_NUMBER"
+		echo "Got version: $VERSION"
+		echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
+	fi
+elif [[ "$GITHUB_EVENT_NAME" == "$pull_request_string" ]]; then
 	echo "Run for pull_request" 
 	echo "Got pre-release: pr-$PR_NUMBER"
 	last_tag=$(git describe --tags --abbrev=0 --match="calendarbot/*")
@@ -17,5 +31,8 @@ else
 	VERSION="$(echo $last_tag | rev | cut -d'/' -f1 | rev)-pr-$PR_NUMBER"
 	echo "Got version: $VERSION"
 	echo "VERSION=$VERSION" >> $GITHUB_OUTPUT
+else
+	echo "No event name found"
+	exit 1
 fi
 
