06640abe0dd117755e3c42ebeebde114434382f3
yoyozbi <46626633+yoyozbi@users.noreply.github.com>
2024-12-20 15:02:06 +0100

    change some things

--- crates/calendarbot/src/discord/commands/calendar/set.rs
+++ crates/calendarbot/src/discord/commands/calendar/set.rs
@@ -1,10 +1,66 @@
+use crate::models::GuildCalendar;
 use crate::schema::guilds_calendars::dsl as guilds_calendars;
 use crate::types::TimezoneChoices;
 use crate::ApplicationContext;
 use anyhow::Result;
 use diesel::prelude::*;
-use diesel_async::RunQueryDsl;
+use diesel_async::{AsyncPgConnection, RunQueryDsl};
 use log::trace;
+
+async fn update_settings(
+    db: &mut AsyncPgConnection,
+    channel_id: u64,
+    timezone: Option<String>,
+    nb_displayed_days: Option<i32>,
+    skip_weekends: Option<bool>,
+    skip_empty_days: Option<bool>,
+) -> Result<()> {
+    let default_values = guilds_calendars::guilds_calendars
+        .filter(guilds_calendars::channelId.eq(channel_id.to_string()))
+        .first::<GuildCalendar>(db)
+        .await;
+
+    if let Err(e) = default_values {
+        return Err(e.into());
+    }
+
+    let mut values = default_values.unwrap();
+
+    if let Some(timezone) = timezone {
+        values.timezone = timezone;
+    }
+
+    if let Some(nb_displayed_days) = nb_displayed_days {
+        values.nbDisplayedDays = nb_displayed_days;
+    }
+
+    if let Some(skip_weekends) = skip_weekends {
+        values.skipWeekend = skip_weekends;
+    }
+
+    if let Some(skip_empty_days) = skip_empty_days {
+        values.skipEmptyDays = skip_empty_days;
+    }
+
+    let res = diesel::update(
+        guilds_calendars::guilds_calendars
+            .filter(guilds_calendars::channelId.eq(channel_id.to_string())),
+    )
+    .set((
+        guilds_calendars::timezone.eq(&values.timezone),
+        guilds_calendars::nbDisplayedDays.eq(values.nbDisplayedDays),
+        guilds_calendars::skipWeekend.eq(values.skipWeekend),
+        guilds_calendars::skipEmptyDays.eq(values.skipEmptyDays),
+        guilds_calendars::forceUpdate.eq(true),
+    ))
+    .execute(db)
+    .await;
+
+    match res {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.into()),
+    }
+}
 
 #[poise::command(
     slash_command,
@@ -41,29 +97,27 @@ pub async fn timezone(
             _ => return Err(e.into()),
         }
     }
+    let old_timezone = res.unwrap();
+
+    if old_timezone == timezone {
+        let _ = ctx.reply("Timezone already set to this value").await?;
+        return Ok(());
+    }
 
     trace!(
         "Changing timezone from {:?} to {:?} for channel {:?}",
-        res.unwrap(),
+        old_timezone,
         timezone,
         channel.id.get()
     );
-    let res = diesel::update(
-        guilds_calendars::guilds_calendars
-            .filter(guilds_calendars::channelId.eq(channel.id.get().to_string())),
-    )
-    .set((
-        guilds_calendars::timezone.eq(timezone),
-        guilds_calendars::forceUpdate.eq(true),
-    ))
-    .execute(&mut db)
-    .await;
+
+    let res = update_settings(&mut db, channel.id.get(), Some(timezone), None, None, None).await;
 
     match res {
         Ok(_) => {
             let _ = ctx.reply("Timezone updated").await?;
             Ok(())
         }
-        Err(e) => Err(e.into()),
+        Err(e) => Err(e),
     }
 }
